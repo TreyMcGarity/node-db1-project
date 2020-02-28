6aa89df6c9961b924c7a80086fe3a2c9ebcac79f
Trey McGarity <trey.mcgarity99@gmail.com>
2020-02-28 10:38:53 -0600

    on github

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
+const accountRouter = require('./account-router');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 module.exports = server;
